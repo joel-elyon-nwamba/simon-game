9394eb7a3e74ea9bd51109add8bf275f0b01d794
joel-nwamba <joel.elyon.nwamba@gmail.com>
2023-09-09 17:52:06 -0400

    grabbed all DOM to initiate the game flow

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// First grab all DOMS
+const startButton = document.querySelector(".start-button");
+const readyToPlay = document.querySelector(".ready-to-play");
+const score = document.getElementById("score");
+const highestScore = document.getElementById("highest-score");
+const playNewGame = document.getElementById("play-new-game");
+const resetGame = document.getElementById("reset-game");
+
+// Grab all DOM color sequence change
+const greenColor = documnent.querySelector(".top-left-panel-green");
+const yellowColor = document.querySelector(".top-right-panel-yellow");
+const redColor = document.querySelector(".bottom-left-panel-red");
+const blueColor = document.querySelector(".bottom-right-panel-blue");

82354afdd5840fa1547cc120d8d0e073c3fb4f1d
joel-nwamba <joel.elyon.nwamba@gmail.com>
2023-09-11 11:15:07 -0400

    Added the simon sequence

--- js/index.js
+++ js/index.js
@@ -11,3 +11,22 @@ const greenColor = documnent.querySelector(".top-left-panel-green");
 const yellowColor = document.querySelector(".top-right-panel-yellow");
 const redColor = document.querySelector(".bottom-left-panel-red");
 const blueColor = document.querySelector(".bottom-right-panel-blue");
+
+// When user clicks on start the game the sequence of the game should start
+// if the first random color appear, play should begin following the pattern
+// if player successfully follows pattern the score increases
+// Understand my problem: I want to be ablw to have the Simon game be able to flash the colors, and to follow a sequence, and also for the player to be able follow the sequence.
+const simonColor = ["green", "red", "yellow", "blue"];
+let gameStart = false;
+let simonPattern = [];
+let userInputPatter = [];
+let scoreBoard = 0;
+
+function simonSequence() {
+  const randomColor = Math.floor(Math.random() * simonColor.length);
+  simonPattern.push(randomColor);
+}
+
+function flshColor() {
+  
+}

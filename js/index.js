c12e37f63f38d7d1f93857896f2bc350bc4332b8
joel-nwamba <joel.elyon.nwamba@gmail.com>
2023-09-20 00:07:19 -0400

    managing the code in smaller chunks

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
-// Grab DOM
 const startButton = document.getElementById("start-button");
-const readyToPlay = document.getElementById("ready-to-play");
-const socre = document.getElementById("score");
-const resetGame = document.getElementById("reset-game");
 
-// defining variables
-const buttonColors = ["red", "blue", "green", "yellow"];
 let gameStart = false;
-let simonPattern = [];
-let userInputPattern = [];
-let scoreLevel = 0;
+function startGame() {
+  gameStart = true;
+}
+
+startButton.addEventListener("click", () => {
+  if(gameStart === true) {
+    startGame()
+  } else {
+    return false;
+  }
+});
 
 
 
